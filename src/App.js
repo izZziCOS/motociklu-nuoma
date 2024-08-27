f9384f2c34dd2f4e803ec9b1655b114cf0b76107
izZziCOS <50332439+izZziCOS@users.noreply.github.com>
2024-08-27 19:34:55 +0300

    Update images and add image text grid

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import Header from "./Main/Header"
 import Box from "@mui/material/Box"
 import Footer from "./Main/Footer"
 import SplitContentSwitcher from "./Reusable/SplitContentSwitcher"
+import ImageTextGrid from "./Reusable/ImageTextGrid"
+
+const contentData = [
+  {
+    imageSrc: `${process.env.PUBLIC_URL}/crash.jpg`,
+    text: "Motociklo KASKO draudimas",
+  },
+  {
+    imageSrc: `${process.env.PUBLIC_URL}/life-insurance.jpg`,
+    text: "Vairuotojo sveikatos draudimas",
+  },
+  {
+    imageSrc: `${process.env.PUBLIC_URL}/insurance.jpg`,
+    text: "Privalomasis vairuotojų civilinės atsakomybės draudimas",
+  },
+]
 
 function App() {
   return (
@@ -12,8 +28,8 @@ function App() {
       <div className="main-content">
         <Box
           component="img"
-          src={`${process.env.PUBLIC_URL}/main-display.gif`}
-          alt="Animated GIF"
+          src={`${process.env.PUBLIC_URL}/main-display.jpg`}
+          alt="Background image"
           className="main-image"
         />
         <div className="overlay-text-small">
@@ -22,10 +38,13 @@ function App() {
         </div>
         <div className="overlay-text-big">JŪSŲ NUOTYKIAI PRASIDEDA ČIA</div>
         <div className="content-switcher-container">
-          <div className="overlay-text-big">Nuomojami motociklai</div>
+          <div className="overlay-text-big-white">Nuomojami motociklai</div>
           <SplitContentSwitcher />
         </div>
       </div>
+
+      <div className="text-big">Suteikiami draudimai</div>
+      <ImageTextGrid items={contentData} />
       <Footer />
     </div>
   )
